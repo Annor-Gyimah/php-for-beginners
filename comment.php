28fd8898c112d380414087dc95b455ed86df6404
Annor-Gyimah <eannor707@gmail.com>
2023-07-26 12:08:54 +0000

    first-commit

---
+++ comment.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "This is my official studies for PHP"
+// This line is a comment
+/*
+This is a multi-comment
+so which means you can type anything here in-between
+*/
+
+?>
---
+++ variables.php
@@ -0,0 +1,26 @@
+<?php
+    $name = "Annorion";
+
+    echo "Hello, {$name} <br>";
+
+    $hobby = "coding";
+
+    echo "You like {$hobby} <br>";
+
+    $age = 23;
+
+    echo "You are {$age} years old <br>";
+    
+    $users = 2;
+    echo "There are {$users} users online <br>";
+    
+    $quaty = 3;
+    echo "You will like to buy {$quaty} items <br>";
+    
+    $gpa = 3.6;
+    echo "Your GPA is {$gpa} <br>";
+    $price = 4.99;
+    echo "Your pizza is {$price}"
+
+    
+?>
